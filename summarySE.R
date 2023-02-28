1b0c43e530bfcf2f6c4a59bb99597a51448b5e79
Joan Borràs-Comes <ebrenc@gmail.com>
2023-02-28 13:07:22 +0100

    Update summarySE.R

--- summarySE.R
+++ summarySE.R
@@ -1,24 +1,24 @@
-summarySE = function(data = NULL, measurevar, groupvars = NULL, na.rm = T, conf.interval = .95, .drop = TRUE) {
+summarySE = function(data = NULL, measurevar, groupvars = NULL, na.rm = TRUE, conf.interval = .95, .drop = TRUE) {
   library(dplyr)
   
   n_custom = function(x) {sum(!is.na(x))}
   mode_custom = function(x) {x = x[!is.na(x)]; ux = unique(x); ux[which.max(tabulate(match(x, ux)))]}
   
-  aggdata = group_by_at(.tbl = data, .vars = groupvars) %>% 
-    summarise(across(.cols = all_of(measurevar), .names = "{fn}",
-                     .fns = list(
-                       n = ~n_custom(.), 
-                       mode = ~mode_custom(.), 
-                       median = ~median(., na.rm = TRUE), 
-                       min = ~min(., na.rm = TRUE), 
-                       max = ~max(., na.rm = TRUE), 
-                       mean = ~mean(., na.rm = TRUE), 
-                       sd = ~sd(., na.rm = TRUE), 
-                       q25 = ~quantile(., .25, na.rm = TRUE), 
-                       q75 = ~quantile(., .75, na.rm = TRUE), 
-                       q1_3 = ~quantile(., (1/3), na.rm = TRUE), 
-                       q2_3 = ~quantile(., (2/3), na.rm = TRUE)
-                     ))) %>% 
+  aggdata = data %>%
+    group_by(across(all_of(groupvars))) %>%
+    summarise(
+      n = n_custom(!!sym(measurevar)),
+      mode = mode_custom(!!sym(measurevar)),
+      median = median(!!sym(measurevar), na.rm = na.rm),
+      min = min(!!sym(measurevar), na.rm = na.rm),
+      max = max(!!sym(measurevar), na.rm = na.rm),
+      mean = mean(!!sym(measurevar), na.rm = na.rm),
+      sd = sd(!!sym(measurevar), na.rm = na.rm),
+      q25 = quantile(!!sym(measurevar), 0.25, na.rm = na.rm),
+      q75 = quantile(!!sym(measurevar), 0.75, na.rm = na.rm),
+      q1_3 = quantile(!!sym(measurevar), 1/3, na.rm = na.rm),
+      q2_3 = quantile(!!sym(measurevar), 2/3, na.rm = na.rm)
+    ) %>%
     mutate(se = sd / sqrt(n)) %>%
     mutate(ci = se * (qt(conf.interval/2 + .5, n-1))) %>%
     ungroup()
