6117a26393c4e2d2ab492953cd7e9560a2633833
Joan Borràs-Comes <ebrenc@gmail.com>
2023-03-02 09:08:58 +0100

    Update var_normalize.R

--- var_normalize.R
+++ var_normalize.R
@@ -1,21 +1,10 @@
-var_normalize = function(x, poly_n = 3, print_coefs = F) {
+var_normalize = function(x, poly_n = 3, get_coefs = F) {
   require(tidyverse)
   x = x + abs(min(x)) + 1
   ordernorm = x %>% bestNormalize::orderNorm(warn = F) %>% pluck("x.t")
   fit = lm(ordernorm ~ poly(log10(x), poly_n, raw = T), NULL)
-  if (print_coefs == T) {print(coef(fit))}
+  poly = poly(log10(x), poly_n, raw = T) %>% as_tibble() %>% pluck("1")
   normalized = predict(fit, as.data.frame(x))
-  return(normalized)
+  if (get_coefs == T) {result = list(norm = normalized, coef = enframe(coef(fit), name = NULL) %>% rename(coef = value), poly = poly)} else {result = normalized}
+  return(result)
 }
-
-# set.seed(123)
-# df = bind_rows(rgamma( 1500, 3)       %>% enframe() %>% mutate(group = "A", .before = name), 
-#                (rt(    1500, 3)+4)    %>% enframe() %>% mutate(group = "B", .before = name), 
-#                (rgamma(1500, 3)^2-40) %>% enframe() %>% mutate(group = "C", .before = name))
-# df %>% ggplot(aes(value)) + geom_histogram(bins = 50) + facet_grid(.~group, scales = "free")
-# 
-# df = df %>% group_by(group) %>% mutate(value_norm = value %>% var_normalize())
-# 
-# df %>% ggplot(aes(value_norm)) +        geom_histogram() + facet_grid(.~group, scales = "free")
-# df %>% ggplot(aes(value, value_norm)) + geom_point() + facet_grid(.~group, scales = "free")
-# df %>% ggplot(aes(value_norm)) +        geom_histogram()
