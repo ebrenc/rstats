398f2408ef6c34a86e305ec2ac39a04031008b93
Joan Borràs-Comes <ebrenc@gmail.com>
2023-03-02 09:14:17 +0100

    Update var_normalize.R

--- var_normalize.R
+++ var_normalize.R
@@ -1,6 +1,6 @@
 var_normalize = function(x, poly_n = 3, get_coefs = F) {
   require(tidyverse)
-  x = x + abs(min(x)) + 1
+  x = x + abs(min(x, na.rm=T)) + 1
   ordernorm = x %>% bestNormalize::orderNorm(warn = F) %>% pluck("x.t")
   fit = lm(ordernorm ~ poly(log10(x), poly_n, raw = T), NULL)
   poly = poly(log10(x), poly_n, raw = T) %>% as_tibble() %>% pluck("1")
