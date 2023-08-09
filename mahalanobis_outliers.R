c3afc2723f52d5a8d112d02d886144c6640237df
Joan Borràs-Comes <ebrenc@gmail.com>
2023-08-09 21:04:00 +0200

    Create mahalanobis_outliers.R
    Alternative to:
    
    # df2 <- df %>% group_by(Subject, Vowel) %>% group_modify(~ mahalanobis_outliers2(temp = .x, vars = c("B0", "B1")))
    
    mahalanobis_outliers2 <- function(temp, vars, chisq.prob = 0.99) {
      temp_sel <- temp[, vars]
      center <- colMeans(temp_sel, na.rm = TRUE)
      cov_matrix <- cov(temp_sel, use = "pairwise.complete.obs")
      mah <- mahalanobis(temp_sel, center = center, cov = cov_matrix)
      threshold <- qchisq(chisq.prob, length(vars))
      mah_chisq <- mah > threshold
      temp$mah <- mah
      temp$mah_chisq <- mah_chisq
      return(temp)
    }

---
+++ mahalanobis_outliers.R
@@ -0,0 +1,30 @@
+# df1 = df %>% mahalanobis_outliers(vars = c(B0, B1), group_by = c(Subject, Vowel), chisq.prob = 0.99, verbose = T)
+
+mahalanobis_outliers = function(data, vars = NULL, group_by = NULL, chisq.prob = 0.99, act = FALSE, verbose = FALSE) {
+  require(tidyverse)
+  
+  vars_colnames = data %>% select({{vars}}) %>% colnames()
+  data = data %>% 
+    nest(groups = -c({{group_by}}, {{vars_colnames}}), dependent = c({{vars_colnames}})) %>%
+    mutate(mah = map(dependent, ~mahalanobis(.x, center = colMeans(.x, na.rm = TRUE), cov = cov(.x, use = "pairwise.complete.obs")))) %>%
+    unnest(c(groups, dependent, mah))
+  
+  threshold <- qchisq(chisq.prob, length(vars_colnames))
+  data$mah_chisq <- data$mah > threshold
+  
+  if (verbose) {
+    n_outliers = sum(data$mah > threshold)
+    n_rows = length(data$mah)
+    cat(n_outliers, "/", n_rows, "outliers", "\n")
+    cat("Proportion:", n_outliers / n_rows, "\n")
+    cat("Distance threshold:", threshold, "\n")
+  }
+  
+  if (act & length(vars_colnames) == 1) {
+    data = data %>% 
+      mutate(across({{vars_colnames}}, ~ if_else(mah_chisq, NA_real_, .))) %>%
+      select(-c(mah, mah_chisq))
+    }
+  
+  return(data)
+}
